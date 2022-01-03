b36e84b7e82909e8f3030f337b4d8495f0b496a8
LogvnR <logvnjs@gmail.com>
2022-01-03 14:52:41 -0800

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import styles from "./App.module.css";
 import NavBar from "./Components/NavBar";
 import Planet from "./Components/Planet";
 import Background from "./Components/UI/Background";
